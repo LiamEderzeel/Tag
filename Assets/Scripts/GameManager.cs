249735814d97ada69e3a492db6f5008a5317fc3a
LiamEderzeel <liamederzeel@gmail.com>
2015-10-08 16:21:13 +0200

    start on game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public enum GameState {Menu, Game}
+	private static GameState _gameState;
+	public string _selectedLevel = "level_1";
+	
+	void Awake () {
+
+	}
+
+	void Update () {
+		//switch(_gameState)
+		//{
+		//case GameState.Menu:
+		//	break;
+		//case GameState.Game:
+		//	break;
+		//}
+	}
+
+	public void ToMenu ()
+	{
+		ResetState();
+		//_menuTicket.SetActive(true);
+	}
+	
+	public void ToGame ()
+	{
+		ResetState();
+		Application.LoadLevel(_selectedLevel);
+	}
+	
+	private static void ResetState()
+	{
+		//_menuMain.SetActive(false);
+		//_menuTicket.SetActive(false);
+		//_menuInfo.SetActive(false);
+	}
+}
