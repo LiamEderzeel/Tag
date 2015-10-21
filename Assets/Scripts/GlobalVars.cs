61789461ae848a386a1535751e0cf1dc1357bb7b
LiamEderzeel <liamederzeel@gmail.com>
2015-10-21 12:32:36 +0200

    global vars file

---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVars {
+
+    private static float RoundTime
+    {
+        get{ return _roundTime; }
+    }
+    [SerializeField] private static float _roundTime;
+
+}
