d017c7687ed93132b73e926bf9c2c9099f64dbb1
LiamEderzeel <liamederzeel@gmail.com>
2015-11-02 12:47:49 +0100

    Split player behavior from player movment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,23 +7,15 @@ public delegate void Tagger(Player theTagger);
 public class Player : MonoBehaviour
 {
 	public event Tagger _newTagger;
-	[SerializeField] private int controller = 0;
 	public bool Taged
 	{
 		set { _taged = value; }
 	}
 	[SerializeField] private bool _taged;
-	[SerializeField] private bool _controlle = true;
-	private float r_rot;
-	private Vector2 m_Input;
-	private Vector3 m_MoveDir = Vector3.zero;
 	private CollisionFlags m_CollisionFlags;
 	private Transform _transform;
 	private Renderer _renderer;
-	private string[] _horizontal  = {"Horizontal1", "Horizontal2", "Horizontal3", "Horizontal4"};
-	private string[] _vertical  = {"Vertical1", "Vertical2", "Vertical3", "Vertical4"};
-	private GameObject _character;
-	private float _angle;
+    private GameObject _character;
 
 	void Awake ()
 	{
@@ -45,41 +37,6 @@ public class Player : MonoBehaviour
 			{
 				_newTagger (this);
 			}
-		}
-	}
-
-	void FixedUpdate ()
-	{
-		float speed = 20f;
-		float rSpeed = 60f;
-		if(_controlle)
-		{
-			GetInput();
-			Vector3 desiredMove = new Vector3(m_Input.x, 0, m_Input.y);
-			this.gameObject.transform.eulerAngles = new Vector3(0, GlobalVars.MainCamera.transform.eulerAngles.y -90, 0);
-			this.gameObject.transform.Translate(desiredMove * Time.deltaTime * speed);
-		}
-	}
-
-	void GetInput ()
-	{
-		float horizontal = Input.GetAxis(_horizontal[controller]);
-		float vertical = Input.GetAxis(_vertical[controller]);
-		float threhold =0.2f;
-
-		if(horizontal < threhold && horizontal > -threhold){
-			horizontal = 0f;
-		}
-		if(vertical < threhold && vertical > -threhold){
-			vertical = 0f;
-		}
-
-		m_Input = new Vector2(-horizontal, -vertical);
-		_angle = Mathf.Atan2(-horizontal, -vertical) * Mathf.Rad2Deg + GlobalVars.MainCamera.transform.eulerAngles.y - 180;
-		_character.GetComponent<Character>()._angle = _angle;
-		if (m_Input.sqrMagnitude > 1)
-		{
-			m_Input.Normalize();
 		}
 	}
 
--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -3,13 +3,53 @@ using System.Collections;
 
 public class PlayerMovment : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField] private bool _controlle = true;
+	[SerializeField] private int controller = 0;
+	private float r_rot;
+	private Vector2 m_Input;
+	private Vector3 m_MoveDir = Vector3.zero;
+	private string[] _horizontal  = {"Horizontal1", "Horizontal2", "Horizontal3", "Horizontal4"};
+	private string[] _vertical  = {"Vertical1", "Vertical2", "Vertical3", "Vertical4"};
+	private float _angle;
+	private GameObject _character;
+
+    private void Awake ()
+    {
+		_character = this.gameObject.transform.GetChild(0).gameObject;
+    }
+	void FixedUpdate ()
+	{
+		float speed = 20f;
+		float rSpeed = 60f;
+		if(_controlle)
+		{
+			GetInput();
+			Vector3 desiredMove = new Vector3(m_Input.x, 0, m_Input.y);
+			this.gameObject.transform.eulerAngles = new Vector3(0, GlobalVars.MainCamera.transform.eulerAngles.y -90, 0);
+			this.gameObject.transform.Translate(desiredMove * Time.deltaTime * speed);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void GetInput ()
+	{
+		float horizontal = Input.GetAxis(_horizontal[controller]);
+		float vertical = Input.GetAxis(_vertical[controller]);
+		float threhold =0.2f;
+
+		if(horizontal < threhold && horizontal > -threhold){
+			horizontal = 0f;
+		}
+		if(vertical < threhold && vertical > -threhold){
+			vertical = 0f;
+		}
+
+		m_Input = new Vector2(-horizontal, -vertical);
+		_angle = Mathf.Atan2(-horizontal, -vertical) * Mathf.Rad2Deg + GlobalVars.MainCamera.transform.eulerAngles.y - 180;
+		_character.GetComponent<Character>()._angle = _angle;
+		if (m_Input.sqrMagnitude > 1)
+		{
+			m_Input.Normalize();
+		}
 	}
+
 }
