193301e548b6890ffd887837dad279f2346eec68
B1rch <berkhout.jurre@gmail.com>
2015-12-02 23:12:59 +0100

    Player nummers boven de spelers
    heb ff zitten kijken naar de nummers boven de spelers, maar ik krijg het
    niet echt goed...

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	[SerializeField] protected int _controller = 0;
